a23b4e4ed1eac5bbd532ccc1df349d40228805df
Philipp <press.fm@mail.ru>
2022-12-12 12:12:28 +0300

    Выполнил упражнение_01

---
+++ Example_01_cycle/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int res = 1;
+for (int x = 1; x <= b; x++)
+{
+    res = res * a;
+    
+}
+Console.WriteLine($"Число {a} в степени {b} равно {res}");
