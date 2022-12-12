2a16438dfe59008296b60cfd84cdb850e066a4fa
Philipp <press.fm@mail.ru>
2022-12-12 13:26:32 +0300

    Выполнил упражнение_03

---
+++ Example_03_massiv/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите размер массива");
+
+int n = Convert.ToInt32(Console.ReadLine());
+if (n <= 0)
+{
+    Console.WriteLine("Ошибка, число должно быть больше 0. Попробуйте еще.");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+int [] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("Введите число массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
