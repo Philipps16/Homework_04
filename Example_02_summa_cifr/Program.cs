f83af95beeffd053e5986967740e39ecc9c0d6ec
Philipp <press.fm@mail.ru>
2022-12-12 14:30:09 +0300

    Выполнил упражнение_02

--- Example_02_summa_cifr/Program.cs
+++ Example_02_summa_cifr/Program.cs
@@ -1,19 +1,13 @@
 ﻿Console.Clear();
 
 Console.WriteLine("Введите число: ");
-int a = Convert.ToInt32(Console.ReadLine());
-if (a < 10)
-{
-    Console.WriteLine("Это не число, попробуйте еще:");
-    a = Convert.ToInt32(Console.ReadLine());
-}
-string b = a.ToString();
-char [] array = b.ToCharArray();
+string? a = Console.ReadLine();
+int l = a.Length - 1;
+double sum = 0;
 
-int sum = array [0];
-for (int i = 0; i < (array.Length - 2); i++)
+for (int i = 0; i <= l; i++)
 {
-    sum = sum + array[i+1];
+    sum = sum + int.Parse(a[i].ToString());
 }
 
 Console.WriteLine($"Сумма цифр в числе равна: {sum}");
